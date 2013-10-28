e7b556dd76a1972b8d08dbd97780c5fdd2a23d83
Marcel Cojocaru <marcel.cojocaru@threepillarglobal.com>
2013-10-28 12:20:54 +0200

    LUN-1169 -- hide fileds that should not confuse the user. The user has to set only the width and the height constrainst

--- cropduster/admin.py
+++ cropduster/admin.py
@@ -4,25 +4,31 @@ from cropduster.models import Size, SizeSet
 
 class SizeInline(admin.TabularInline):
     model = Size
-    prepopulated_fields = {'slug': ('name',)}
+#    prepopulated_fields = {'slug': ('name',)}
     fieldsets = (
         (None, {
             'fields': (
-                'name',
-                'slug',
+#                'name',
+#                'slug',
                 'width',
                 'height',
-                'auto_crop',
+#                'auto_crop',
                 'size_set',
-                'aspect_ratio',
-                'retina',
+#               'aspect_ratio',
+#                'retina',
             ),
         }),
     )
+    max_num = 1
+    min_num = 1
 
+    def __init__(self, *args, **kwargs):
+        super(SizeInline, self).__init__(*args, **kwargs)
+        self.can_delete = False
 
 class SizeSetAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     inlines = [SizeInline]
 
 admin.site.register(SizeSet, SizeSetAdmin)
+
